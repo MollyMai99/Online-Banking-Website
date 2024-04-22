e13856783012bc676de11b7d1b3b2aca5ed32104
MollyMai99 <miaoling.mai99@gmail.com>
2024-04-22 23:07:59 +0800

    feat: add function to fetch API data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-// import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
   // const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    async function logMovies() {
+      const response = await fetch(
+        // "https://api.marketdata.app/v1/stocks/quotes/AAPL"
+        "https://api.marketdata.app/v1/stocks/quotes/AAPL/?dateformat=timestamp"
+        // "https://api.marketdata.app/v1/stocks/news/AAPL/?dateformat=timestamp&limit=2"
+      );
+      const movies = await response.json();
+      console.log(movies);
+    }
+    logMovies();
+  }, []);
 
   return <>hi</>;
 }

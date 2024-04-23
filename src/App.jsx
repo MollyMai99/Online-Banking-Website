a24b94cc3e767853a6cd0cf811ce9805a74d33ba
MollyMai99 <miaoling.mai99@gmail.com>
2024-04-23 14:49:43 +0800

    feat: add HomePage-bank background

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+import HomePage from "./HomePage/HomePage";
 
 function App() {
   // const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    async function logMovies() {
-      const response = await fetch(
-        // "https://api.marketdata.app/v1/stocks/quotes/AAPL"
-        // "https://api.marketdata.app/v1/stocks/quotes/AAPL/?dateformat=timestamp"
-        "https://api.marketdata.app/v1/stocks/news/AAPL/?dateformat=timestamp&limit=2"
-      );
-      const movies = await response.json();
-      console.log(movies);
-      return movies;
-    }
-    logMovies();
-  }, []);
+  // useEffect(() => {
+  //   async function logMovies() {
+  //     const response = await fetch(
+  //       // "https://api.marketdata.app/v1/stocks/quotes/AAPL"
+  //       // "https://api.marketdata.app/v1/stocks/quotes/AAPL/?dateformat=timestamp"
+  //       "https://api.marketdata.app/v1/stocks/news/AAPL/?dateformat=timestamp&limit=2"
+  //     );
+  //     const movies = await response.json();
+  //     console.log(movies);
+  //     return movies;
+  //   }
+  //   logMovies();
+  // }, []);
 
   return (
     <>
       <h1>BBS</h1>
-      <p>Bank Background</p>
-      <p>Feature Set(Link to each page)</p>
+      <HomePage />
       <p>CardPage</p>
       <p>NewsPage</p>
       <p>StocksPage</p>
---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>Bank Background</h1>
+      <p>
+        Established in 2024, BBS has been a cornerstone of financial stability,
+        offering a spectrum of services tailored to meet the diverse needs of
+        our clientele. With a commitment to innovation and integrity, we foster
+        trust while navigating the evolving landscape of global finance,
+        ensuring prosperity for all stakeholders.
+      </p>
+      <p>Feature Set(Link to each page)</p>
+    </>
+  );
+}

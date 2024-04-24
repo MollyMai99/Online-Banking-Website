64808ef0dae8e537cf019d28a9cf19ed8a39445b
MollyMai99 <miaoling.mai99@gmail.com>
2024-04-24 11:26:43 +0800

    feat:fetch data from API and show data in top stocks table

--- src/StocksPage/StocksPage.jsx
+++ src/StocksPage/StocksPage.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import SavedStocksPage from "./SavedStocksPage";
 import TopStocksPage from "./TopStocksPage";
 
 export default function StocksPage() {
+  const [stocks, setStock] = useState([]);
+
+  useEffect(() => {
+    async function loadStocks() {
+      const response = await fetch(
+        // "https://api.marketdata.app/v1/stocks/quotes/AAPL"
+        // "https://api.marketdata.app/v1/stocks/quotes/AAPL/?dateformat=timestamp"
+        "https://api.marketdata.app/v1/stocks/news/AAPL/?dateformat=timestamp&limit=1"
+        // "https://api.aletheiaapi.com/Crypto?symbol=DOGE&key=CF7704FC00CD4518B2316C46C9DD77B7"
+        // "https://api.aletheiaapi.com/Crypto?symbol=BTC&key=CF7704FC00CD4518B2316C46C9DD77B7"
+        // "https://api.aletheiaapi.com/Crypto?symbol=XRP&key=CF7704FC00CD4518B2316C46C9DD77B7"
+        // "https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=jpqEm77zS2gsDoy1tifSsrgMvvpw3XS2zl6HHf2V"
+        // "https://api.stockdata.org/v1/data/quote?symbols=AAPL&api_token=jpqEm77zS2gsDoy1tifSsrgMvvpw3XS2zl6HHf2V"
+      );
+      const data = await response.json();
+      const stocksData = data.data;
+      setStock(stocksData);
+      //   console.log("1", stocks);
+    }
+    loadStocks();
+  }, []);
+
   return (
     <>
       <h1>StocksPage</h1>
       <p>Navbar</p>
-      <TopStocksPage />
+      <TopStocksPage stocks={stocks} />
       <SavedStocksPage />
     </>
   );
--- src/StocksPage/TopStocksPage.jsx
+++ src/StocksPage/TopStocksPage.jsx
@@ -1,46 +1,34 @@
-import { useEffect } from "react";
 import StockDetailPage from "./StockDetailPage";
 
-export default function TopStocksPage() {
-  useEffect(() => {
-    async function logStocks() {
-      const response = await fetch(
-        // "https://api.marketdata.app/v1/stocks/quotes/AAPL"
-        "https://api.marketdata.app/v1/stocks/quotes/AAPL/?dateformat=timestamp"
-        // "https://api.marketdata.app/v1/stocks/news/AAPL/?dateformat=timestamp&limit=1"
-        // "https://api.aletheiaapi.com/Crypto?symbol=DOGE&key=CF7704FC00CD4518B2316C46C9DD77B7"
-        // "https://api.aletheiaapi.com/Crypto?symbol=BTC&key=CF7704FC00CD4518B2316C46C9DD77B7"
-        // "https://api.aletheiaapi.com/Crypto?symbol=XRP&key=CF7704FC00CD4518B2316C46C9DD77B7"
-        // "https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=jpqEm77zS2gsDoy1tifSsrgMvvpw3XS2zl6HHf2V"
-        // "https://api.stockdata.org/v1/data/quote?symbols=AAPL&api_token=jpqEm77zS2gsDoy1tifSsrgMvvpw3XS2zl6HHf2V"
-      );
-      const stocks = await response.json();
-      console.log(stocks);
-      return stocks;
-    }
-    logStocks();
-  }, []);
+export default function TopStocksPage({ stocks }) {
+  //   console.log("top", stocks);
 
   return (
     <>
       <table>
         <thead>
           <tr>
+            <th>Name</th>
             <th>Symbol</th>
             <th>Currency</th>
             <th>Price</th>
+            <th>Last Tarde Time</th>
             <th>Save</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>AAPL</td>
-            <td>USD</td>
-            <td>123</td>
-            <td>
-              <button>Save</button>
-            </td>
-          </tr>
+          {stocks.map((stock) => (
+            <tr key={stock.ticker}>
+              <td>{stock.name}</td>
+              <td>{stock.ticker}</td>
+              <td>{stock.currency}</td>
+              <td>{stock.price}</td>
+              <td>{stock.last_trade_time}</td>
+              <td>
+                <button>Save</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <StockDetailPage />

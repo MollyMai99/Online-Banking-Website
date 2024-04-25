a979714aabdd5c7578dd8f451e21a9af11643be0
MollyMai99 <miaoling.mai99@gmail.com>
2024-04-25 11:11:19 +0800

    erase API->save request quota

--- src/Pages/StocksPage.jsx
+++ src/Pages/StocksPage.jsx
@@ -8,12 +8,14 @@ export default function StocksPage() {
   useEffect(() => {
     async function loadStocks() {
       const response = await fetch(
+        "https://api.marketdata.app/v1/stocks/quotes/AAPL"
         // "https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=jpqEm77zS2gsDoy1tifSsrgMvvpw3XS2zl6HHf2V"
 
-        "https://api.stockdata.org/v1/data/quote?symbols=AAPL&api_token=jpqEm77zS2gsDoy1tifSsrgMvvpw3XS2zl6HHf2V"
+        // "https://api.stockdata.org/v1/data/quote?symbols=AAPL&api_token=jpqEm77zS2gsDoy1tifSsrgMvvpw3XS2zl6HHf2V"
         // "https://api.stockdata.org/v1/data/quote?symbols=AAPL&api_token=JgOAADvWjnurMD8QLuMAkLF5XlL7pD8jQUMCqXlC"
         // "https://api.stockdata.org/v1/data/quote?symbols=AAPL&api_token=SQ6IJwKFCd5COSkR2TSgYxA4RCV0fzStMaVwPFSB"
       );
+
       const data = await response.json();
       const stocksData = data.data;
       setStocks(stocksData);

36255ea26014cf4e1f7e85955ebac48f0dc47bf7
MollyMai99 <miaoling.mai99@gmail.com>
2024-04-29 18:29:55 +0800

    feat:spilt component:UnsaveService.jsx

--- src/Pages/StocksPage/SavedStocksPage.jsx
+++ src/Pages/StocksPage/SavedStocksPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import UnsaveService from "./UnsaveService";
 
 export default function SavedStocksPage() {
   const [savedList, setSavedList] = useState([]);
@@ -27,25 +27,7 @@ export default function SavedStocksPage() {
     loadSavedList();
   }, []);
 
-  async function deleteStock(id) {
-    const url = `https://api.airtable.com/v0/appu6JDxJ0kPqISCM/Table%201/${id}`;
-    const key =
-      "patc1K5wmYJTJIkjP.febf10fd5d52335a34877cd8e8eb742c0d247d1e0e9b86fc70259d7a20c4e076";
-
-    const options = {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${key}`,
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch(url, options);
-    await response.json();
-
-    deleteStockState(id);
-  }
-
-  function deleteStockState(id) {
+  function unsaveStockState(id) {
     console.log("deletestate", id);
     const index = savedList.findIndex((stock) => stock.id === id);
     setSavedList([...savedList.slice(0, index), ...savedList.slice(index + 1)]);
@@ -53,17 +35,39 @@ export default function SavedStocksPage() {
 
   return (
     <>
-      <p>SavedStocksPage</p>
-      {savedList.map((savedStock) => (
-        <ul key={savedStock.fields.Symbol}>
-          <li>
-            <Link to={`/stocks/${savedStock.fields.Symbol}`}>
-              {savedStock.fields.Symbol}
-            </Link>
-            <button onClick={() => deleteStock(savedStock.id)}>Unsave</button>
-          </li>
-        </ul>
-      ))}
+      <p>Stocks you saved:</p>
+      <UnsaveService
+        savedList={savedList}
+        unsaveStockState={unsaveStockState}
+      />
     </>
   );
 }
+
+// async function unsaveStock(id) {
+//   const url = `https://api.airtable.com/v0/appu6JDxJ0kPqISCM/Table%201/${id}`;
+//   const key =
+//     "patc1K5wmYJTJIkjP.febf10fd5d52335a34877cd8e8eb742c0d247d1e0e9b86fc70259d7a20c4e076";
+
+//   const options = {
+//     method: "DELETE",
+//     headers: {
+//       Authorization: `Bearer ${key}`,
+//       "Content-Type": "application/json",
+//     },
+//   };
+//   const response = await fetch(url, options);
+//   await response.json();
+
+//   unsaveStockState(id);
+// }
+
+/* {/* {savedList.map((savedStock) => (
+  <UnsaveHandle
+    key={savedStock.id}
+    id={savedStock.id}
+    symbol={savedStock.fields.Symbol}
+    // savedList={savedList}
+    // setSavedList={setSavedList}
+    unsaveStockState={unsaveStockState}
+  /> */

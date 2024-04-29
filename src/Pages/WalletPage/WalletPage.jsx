eaa8d12dca9952d45ce71dd93fa0d480c6195a74
MollyMai99 <miaoling.mai99@gmail.com>
2024-04-29 11:04:22 +0800

    feat:fill up f()handleDeposit

--- src/Pages/WalletPage/WalletPage.jsx
+++ src/Pages/WalletPage/WalletPage.jsx
@@ -5,8 +5,8 @@ export default function WalletPage() {
 
   useEffect(() => {
     async function loadWallet() {
-      const url =
-        "https://api.airtable.com/v0/appu6JDxJ0kPqISCM/wallet?maxRecords=3&view=Grid%20view";
+      const url = "https://api.airtable.com/v0/appu6JDxJ0kPqISCM/wallet";
+      // "https://api.airtable.com/v0/appu6JDxJ0kPqISCM/wallet?maxRecords=3&view=Grid%20view"
       const key =
         "patc1K5wmYJTJIkjP.febf10fd5d52335a34877cd8e8eb742c0d247d1e0e9b86fc70259d7a20c4e076";
 
@@ -20,15 +20,39 @@ export default function WalletPage() {
       const response = await fetch(url, options);
       const airtableResponse = await response.json();
       const balance = airtableResponse.records[0].fields.balance;
-      console.log(balance);
+      //   console.log(balance);
       setBalance(balance);
       // console.log("savedList", savedList);
     }
     loadWallet();
   }, []);
 
-  function handleDeposit() {
+  async function handleDeposit() {
     console.log("deposit");
+    const url = `https://api.airtable.com/v0/appu6JDxJ0kPqISCM/wallet`;
+    const key =
+      "patc1K5wmYJTJIkjP.febf10fd5d52335a34877cd8e8eb742c0d247d1e0e9b86fc70259d7a20c4e076";
+    const addSymbolObj = {
+      records: [
+        {
+          id: "rec258SyyTyYDcTUW",
+          fields: {
+            balance: "1000",
+          },
+        },
+      ],
+    };
+
+    const options = {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${key}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addSymbolObj),
+    };
+    const response = await fetch(url, options);
+    await response.json();
   }
 
   return (
@@ -37,7 +61,7 @@ export default function WalletPage() {
       <p>Balance: {balance}</p>
       <hr />
       {/* <p>Deposit Funds</p> */}
-      <button onClick={handleDeposit(500)}>Deposit 500 SGD</button>
+      <button onClick={() => handleDeposit()}>Deposit 500 SGD</button>
     </>
   );
 }
